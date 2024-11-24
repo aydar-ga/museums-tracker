587b04c9615772f9dccee42c7e428823fedf941e
aydar-ga <aydarcyber@gmail.com>
2024-11-24 21:51:49 +0100

    added port info

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -372,4 +373,7 @@ def mark_visitwd(id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(
+        os.environ.get("PORT", 5000)
+    )  # Use the PORT env variable or default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
